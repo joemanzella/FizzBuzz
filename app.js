f0c7bb7fb817a108cc206d28c83b20522b232522
joemanzella <josephmanzella@gmail.com>
2015-09-07 18:19:31 -0400

    FizzBuzz Refactor
    Initial upload

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-//while x<101
-//increment
-//IF
-//x%3=0 AND x%5=0, FizzBuzz
-//elseif x%3, Fizz
-//elseif x%5, Buzz
-//Append to body
-
-var theCount;
 $(document).ready(function(){
-	$(document).keydown(function(e) {
-		    if (e.keyCode == 88 || 13 || 32) {
-				for (theCount = 1; theCount < 101; theCount++) {
-				if (theCount % 3 == 0 && theCount % 5 == 0) {
-					$( ".buzzhere" ).append( "<p>FizzBuzz</p>" );
-				} else if (theCount % 3 == 0) {
-					$( ".buzzhere" ).append( "<p>Fizz</p>" );
-				} else if (theCount % 5 == 0) {
-					$( ".buzzhere" ).append( "<p>Buzz</p>" );
-				} else {
-					$( ".buzzhere" ).append( '<p>' + theCount + '</p>' );
-				};
-			}
-		}
-	})
+	var unparsedNumber = prompt("Please enter an integer.");
+	var endNumber = parseInt( unparsedNumber ); //There's no need to filter out decimals. 16.999~ is still less than but NOT equal to 17, so it will not move to the next number.
+	if (isNaN(endNumber) == true) {
+		alert("You didn't enter an integer. Please enter an Integer next time.");
+		var unparsedNumber = prompt("Please enter an integer.");
+		var endNumber = parseInt( unparsedNumber );
+		fizzAndBuzz(endNumber);
+	console.log(endNumber);
+	} else {
+		fizzAndBuzz(endNumber);	
+	}
 });
+
+function fizzAndBuzz(endNumber) {
+	for (var i = 1; i <= endNumber; i++) {
+		if (i % 3 == 0 && i % 5 == 0) { $( ".buzzhere" ).append( "<p>FizzBuzz</p>" );
+		} else if (i % 3 == 0) { 
+			$( ".buzzhere" ).append( "<p>Fizz</p>" );
+		} else if (i % 5 == 0) { 
+			$( ".buzzhere" ).append( "<p>Buzz</p>" );
+		} else { 
+			$( ".buzzhere" ).append( '<p>' + i + '</p>' );
+		};
+	};
+}
+
+
